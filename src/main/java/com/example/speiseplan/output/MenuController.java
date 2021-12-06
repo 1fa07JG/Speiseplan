9cab68722c66e578ba810496ae1993b4af714512
1fa07JG <1fao7-gfi-2020@mail.de>
2021-12-06 13:37:05 +0100

    Import Pdf creater

--- src/main/java/com/example/speiseplan/output/MenuController.java
+++ src/main/java/com/example/speiseplan/output/MenuController.java
@@ -22,6 +22,7 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
+import static com.example.speiseplan.output.CreatePdfMenu.producePdfMenu;
 
 
 public class MenuController {
